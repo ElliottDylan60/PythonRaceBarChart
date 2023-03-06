4a0ec22a41c04c9cc96b557a1993eb9cef28d4c2
CamarataM <CamarataM@cwu.edu>
2023-03-05 16:26:13 -0800

    Implement stacking transparent images using Tkinter
    Will look into a PIL alternative, as Tkinter's Canvas's place_image() seems to have some wierd behaviour for stacking transparent images.

--- bar_chart_race_juxtaposition.py
+++ bar_chart_race_juxtaposition.py
@@ -15,6 +15,7 @@ print("Frames: " + str(len(my_raceplot_plot.frames)))
 frames_folder = "frames"
 os.makedirs(frames_folder, exist_ok=True)
 
+# Taken From 3/5/2023 4:03 PM: https://stackoverflow.com/a/73788157
 # generate images for each step in animation
 frames = []
 for s, fr in enumerate(my_raceplot_plot.frames):
@@ -23,9 +24,6 @@ for s, fr in enumerate(my_raceplot_plot.frames):
 	# move slider to correct place
 	my_raceplot_plot.layout.sliders[0].update(active=s)
 	# generate image of current state
-	# frames.append(Image.open(io.BytesIO(my_raceplot_plot.to_image(format="png"))))
-	# frames.append(my_raceplot_plot.to_image(format="png"))
-
 	file_name = frames_folder + os.sep + "frame" + str(len(frames)) + ".png"
 	my_raceplot_plot.write_image(file=file_name, format="png")
 	frames.append(file_name)
@@ -33,18 +31,14 @@ for s, fr in enumerate(my_raceplot_plot.frames):
 # create animated GIF
 # frames[0].save("test.gif", save_all=True, append_images=frames[1:], optimize=True, duration=500, loop=0)
 
-max_frames = 2
-if __name__ == "__main__":
-	window = Tk()
-	window.title("Bar Chart Race Juxtaposition")
-		
-	canvas = Canvas(window, width=1600, height=900, bg='lightGrey')
-	canvas.images = []
-	canvas.pack()
-	for image_file_path in frames:
-		# can.create_image(150, 150, image=ImageTk.PhotoImage(data=io.BytesIO(image)), format="png")
-		# can.create_image(150, 150, image=PhotoImage(data=io.BytesIO(image)), format="png")
-		# canvas.create_image(0, 0, image=PhotoImage(data=image, format="png"))
+def juxtapose(canvas : Canvas, frames : list[str], x_step_offset = 100, y_step_offset = -100, opacity_step_offset = -100):
+	current_x_offset = 0
+	current_y_offset = 0
+	current_opacity_offset = 0
+
+	for image_file_path_index in range(len(frames)):
+	# for image_file_path_index in reversed(range(len(frames))):
+		image_file_path = frames[image_file_path_index]
 
 		# Taken From 3/4/2023 4:06 PM: https://stackoverflow.com/a/765829
 		img = Image.open(image_file_path)
@@ -53,20 +47,35 @@ if __name__ == "__main__":
 		pixdata = img.load()
 
 		width, height = img.size
+		# if image_file_path_index != 0:
 		for y in range(height):
 			for x in range(width):
-				if pixdata[x, y] == (255, 255, 255, 255):
+				pixel_color_rgba = pixdata[x, y]
+
+				# Make all white pixels in the image transparent.
+				if pixel_color_rgba == (255, 255, 255, 255):
 					pixdata[x, y] = (255, 255, 255, 0)
-				# pixdata[x, y] = (255, 255, 255, random.randint(0, 255))
-				# pixdata[x, y] = (255, 255, 255, 255)
-				# pixdata[x, y] = (pixdata[x, y][0], pixdata[x, y][1], pixdata[x, y][2], random.randint(0, 255))
+				else:
+					pixdata[x, y] = (pixel_color_rgba[0], pixel_color_rgba[1], pixel_color_rgba[2], min(pixel_color_rgba[3] + current_opacity_offset, 255))
 
 		photo_image = ImageTk.PhotoImage(img)
-		# canvas.create_image(random.randint(400, 1200), random.randint(400, 1200), image=photo_image)
-		canvas.create_image(random.randint(400, 500), random.randint(400, 500), image=photo_image)
+		canvas.create_image(current_x_offset + (width / 2), current_y_offset + (height / 2), image=photo_image)
 		canvas.images.append(photo_image)
 
-		if len(canvas.images) >= max_frames:
-			break
+		current_x_offset += x_step_offset
+		current_y_offset += y_step_offset
+		current_opacity_offset += opacity_step_offset
+
+max_frames = 2
+if __name__ == "__main__":
+	window = Tk()
+	window.title("Bar Chart Race Juxtaposition")
+		
+	# canvas = Canvas(window, width=1600, height=900, bg='lightGrey')
+	canvas = Canvas(window, width=1600, height=900, bg='white')
+	canvas.images = []
+	canvas.pack()
+
+	juxtapose(canvas, frames[:3])
 
 	window.mainloop()
